96f1867013931d8b35d094533583bda22d52d266
vaishnavi asthana <vaishnaviasthana369@gmail.com>
2026-04-15 00:28:09 +0530

    Update api_realtime.py

--- src/api_realtime.py
+++ src/api_realtime.py
@@ -567,4 +567,5 @@ def update_sub(req: SubscriptionUpdate, authorization: str = Header(None)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("src.api_realtime:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("src.api_realtime:app", host="0.0.0.0", port=port)
